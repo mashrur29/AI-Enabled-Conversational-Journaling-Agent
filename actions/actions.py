623292cb738c7162bbd8520f7c0bc121aacc2c3e
Mashrur Rashik <mashrur639@gmail.com>
2023-04-02 14:59:00 -0400

    init

---
+++ actions/actions.py
@@ -0,0 +1,136 @@
+from typing import Any, Text, Dict, List
+
+from rasa.core.actions.forms import FormAction
+from rasa_sdk import Action, Tracker, FormValidationAction
+from rasa_sdk.executor import CollectingDispatcher
+from actions.dicts import intent2Symptom, symptom2Slots
+from rasa_sdk.events import SlotSet, ActionReverted, AllSlotsReset
+from rasa_sdk.events import UserUtteranceReverted
+from rasa_sdk.types import DomainDict
+
+class ActionRevertUserUtterance(Action):
+
+    def name(self) -> Text:
+        return "action_revert_user_utterance"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        return [UserUtteranceReverted()]
+
+class ActionResetAllSlot(Action):
+
+    def name(self) -> Text:
+        return "action_reset_slots"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        return [AllSlotsReset()]
+
+class ActionDefaultFallback(Action):
+    """Executes the fallback action and goes back to the previous state
+    of the dialogue"""
+
+    def name(self) -> Text:
+        return "action_default_fallback"
+
+    async def run(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(response="utter_please_rephrase")
+
+        # Revert user message which led to fallback.
+        return [UserUtteranceReverted()]
+
+
+class ActionRepeatQuestion(Action):
+    """Executes the fallback action and goes back to the previous state
+    of the dialogue"""
+
+    def name(self) -> Text:
+        return "action_repeat_question"
+
+    async def run(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(response="utter_repeat_question")
+
+        is_simplify = tracker.get_slot('is_simplify')
+        if is_simplify:
+            return []
+
+        latest_bot_message = ''
+        for event in tracker.events:
+            if (event.get("event") == "bot") and (event.get("event") is not None):
+                latest_bot_message = event.get("text")
+        dispatcher.utter_message(text=latest_bot_message)
+
+        return [UserUtteranceReverted()]
+
+
+class ActionSetSimplify(Action):
+
+    def name(self) -> Text:
+        return "action_set_simplify"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        return [SlotSet("is_simplify", True)]
+
+class ActionResetSimplify(Action):
+
+    def name(self) -> Text:
+        return "action_reset_simplify"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        return [SlotSet("is_simplify", False)]
+
+class ActionSetSymptom(Action):
+
+    def name(self) -> Text:
+        return "action_set_symptom"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        slot_name = tracker.latest_message['intent'].get('name')
+        if slot_name in intent2Symptom:
+            slot_name = intent2Symptom[slot_name]
+
+        return [SlotSet("symptom", slot_name)]
+
+
+class ActionSetSlot(Action):
+
+    def name(self) -> Text:
+        return "action_set_slot"
+
+    def get_next_slot(self, tracker: Tracker):
+        active_loop = tracker.active_loop_name
+        for x in symptom2Slots[active_loop]:
+            if tracker.get_slot(x) is None:
+                return x
+
+        return None
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        user_intent = tracker.latest_message['intent'].get('name')
+
+        next_slot = tracker.get_slot("requested_slot")
+        user_inp = 'None'
+        if next_slot != None:
+            user_inp = tracker.latest_message['text']
+
+        return [SlotSet(next_slot, user_inp)]
---
+++ actions/dicts.py
@@ -0,0 +1,27 @@
+intent2Symptom = {
+    "trigger_tremor_mild": "tremor",
+    "trigger_tremor_severe": "tremor",
+    "trigger_tremor_frequency": "tremor",
+    "trigger_tremor_location": "tremor",
+    "trigger_current_tremors": "tremor",
+    "trigger_bradykinesia_1": "bradykinesia",
+    "trigger_bradykinesia_2": "bradykinesia",
+    "trigger_dizziness": "dizziness",
+    "trigger_falling_1": "falling",
+    "trigger_falling_2": "falling",
+    "trigger_insomnia": "insomnia",
+    "trigger_mood_1": "mood",
+    "trigger_mood_2": "mood",
+    "trigger_mood_3": "mood"
+}
+
+symptom2Slots = {
+    "tremorjournaling": [
+        "medicinetype",
+        "medicinetime",
+        "duration",
+        "cooccurrence",
+        "dailyactivity",
+        "history"
+    ]
+}
